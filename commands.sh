d4de361fc308f80fc7b52133faba155bc29a92b1
Mehmet Tekman <mtekman89@gmail.com>
2024-05-23 17:08:01 +0200

    added unified playbook to call everything

--- commands.sh
+++ commands.sh
@@ -1,3 +1,8 @@
+# Run the whole thing
+ansible-playbook -i inventory.yaml main.yaml --ask-become-pass --extra-vars="install_target=/dev/sdd"
+
 # Build the DietPi image
-ansible-playbook -i inventory.yaml generate-img.yaml --extra-vars="install_target=/dev/sdd" --ask-become-pass # --start-at-task="Set the SSH config"
+##ansible-playbook -i inventory.yaml generate-img.yaml
+##--extra-vars="install_target=/dev/sdd" --ask-become-pass #
+##--start-at-task="Set the SSH config"
 
